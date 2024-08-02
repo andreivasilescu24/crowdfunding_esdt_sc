063a8aa695e9fd794e6121e38b65876e596b7100
Tudor Daian <tudor.daian@student.tuiasi.ro>
2024-08-02 13:39:08 +0300

    ajutoru lui calin nr 1

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,9 +5,10 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
+// Upgrade:                              1
 // Endpoints:                            8
 // Async Callback (empty):               1
-// Total number of exported functions:  10
+// Total number of exported functions:  11
 
 #![no_std]
 
@@ -18,6 +19,7 @@ multiversx_sc_wasm_adapter::endpoints! {
     crowdfunding_esdt
     (
         init => init
+        upgrade => upgrade
         fund => fund
         status => status
         getCurrentFunds => get_current_funds
